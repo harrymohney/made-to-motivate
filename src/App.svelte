f7dc4fa4c42e9312c44d96c1a446b14c40e1c88f
harryhmohney <harrison.mohney@gmail.com>
2023-09-28 13:04:28 -0700

    refactor components

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
   import Quotes from "./components/Quotes.svelte";
+
+	let quotes = [
+		{ id: 1, name: 'Be the change you want to see in the world', selected: true },
+		{ id: 2, name: 'Success is the sum of small efforts - repeated day in and day out.', selected: true },
+		{ id: 3, name: 'Strive not to be a success, but rather to be of value.', selected: true }
+	]
 </script>
 
-<Quotes />
+<Quotes {quotes} />
--- src/components/Quotes.svelte
+++ src/components/Quotes.svelte
@@ -1,16 +1,40 @@
-<h1>Quote Motivation</h1>
-<!-- svelte-ignore a11y-missing-attribute -->
-<img height="32" width="88" src="https://www.w3.org/WAI/wcag2A" />
+<script>
+  export let quotes = []
+  
+  let newQuoteName = ''
+  $: newQuoteId = totalQuotes ? Math.max(...quotes.map(q => q.id)) + 1 : 1
 
+  $: totalQuotes = quotes.length
+  $: selectedQuotes = quotes.filter(quote => quote.selected).length
+
+  function removeQuote(quote) {
+    quotes = quotes.filter(t => q.id !== quote.id)
+  }
+
+  function addQuote() {
+    quotes = [...quotes, { id: newQuoteId, name: newQuoteName, selected: false }]
+    newQuoteName = ''
+  }
+
+  let filter = 'all'
+  const filterQuotes = (filter, quotes) => 
+    filter === 'active' ? quotes.filter(t => !q.selected) :
+    filter === 'selected' ? quotes.filter(t => q.selected) : 
+    quotes
+
+</script>
 
 <!-- Quotes.svelte -->
 <div class="quoteapp stack-large">
+
   <!-- NewQuote -->
-  <form>
+  <form on:submit|preventDefault={addQuote}>
     <h2 class="label-wrapper">
-      <label for="quote-0" class="label__lg"> How motivated are you? </label>
+      <label for="quote-0" class="label__lg">
+        What motivates you?
+      </label>
     </h2>
-    <input type="text" id="quote-0" autocomplete="off" class="input input__lg" />
+    <input bind:value={newQuoteName} type="text" id="quote-0" autocomplete="off" class="input input__lg" />
     <button type="submit" disabled="" class="btn btn__primary btn__lg">
       Add
     </button>
@@ -18,106 +42,63 @@
 
   <!-- Filter -->
   <div class="filters btn-group stack-exception">
-    <button class="btn toggle-btn" aria-pressed="true">
+    <button class="btn toggle-btn" class:btn__primary={filter === 'all'} aria-pressed={filter === 'all'} on:click={()=> filter = 'all'} >
       <span class="visually-hidden">Show</span>
       <span>All</span>
       <span class="visually-hidden">tasks</span>
     </button>
-    <button class="btn toggle-btn" aria-pressed="false">
+    <button class="btn toggle-btn" class:btn__primary={filter === 'active'} aria-pressed={filter === 'active'} on:click={()=> filter = 'active'} >
       <span class="visually-hidden">Show</span>
       <span>Active</span>
-      <span class="visually-hidden">tasks</span>
+      <span class="visually-hidden">Motivation</span>
     </button>
-    <button class="btn toggle-btn" aria-pressed="false">
-      <span class="visually-hidden">Show</span>
-      <span>Completed</span>
+    <button class="btn toggle-btn" class:btn__primary={filter === 'selected'} aria-pressed={filter === 'selected'} on:click={()=> filter = 'selected'} >
+      <span class="visually-hidden">Show</span>    
+      <span>Selected</span>
       <span class="visually-hidden">tasks</span>
     </button>
   </div>
 
-  <!-- QuoteStatus -->
-  <h2 id="list-heading">Let's get motivated</h2>
+  <!-- TodosStatus -->
+  <h2 id="list-heading">{selectedQuotes} out of {totalQuotes} quotes selected</h2>
 
-  <!-- Quotes -->
-  <ul role="list" class="quote-list stack-large" aria-labelledby="list-heading">
-    <!-- quote-1 (editing mode) -->
-    <li class="quote">
-      <div class="stack-small">
-        <form class="stack-small">
-          <div class="form-group">
-            <label for="quote-1" class="quote-label">
-              'Be the change you want to see in the world'
-            </label>
-            <input
-              type="text"
-              id="quote-1"
-              autocomplete="off"
-              class="quote-text" />
-          </div>
-          <div class="btn-group">
-            <button class="btn quote-cancel" type="button">
-              Cancel
-              <span class="visually-hidden">Be the change you want to see in the world</span>
-            </button>
-            <button class="btn btn__primary quote-edit" type="submit">
-              Save
-              <span class="visually-hidden">Be the change you want to see in the world</span>
-            </button>
-          </div>
-        </form>
-      </div>
-    </li>
-
-    <!-- quote-2 -->
-    <li class="quote">
+  <!-- quotes -->
+  <ul role="list" class="quotes-list stack-large" aria-labelledby="list-heading">
+  {#each filterQuotes(filter, quotes) as quotes (quotes.id)}
+    <li class="quotes">
       <div class="stack-small">
         <div class="c-cb">
-          <input type="checkbox" id="quote-2" checked />
-          <label for="quote-2" class="quote-label">
-            Success is the sum of small efforts - repeated day in and day out.
+            <input type="checkbox" id="todo-{quote.id}"
+              on:click={() => quote.completed = !quote.completed}
+              checked={quote.completed}
+            />          
+            <label for="quote-{quote.id}" class="quote-label">
+            {quote.name}
           </label>
         </div>
         <div class="btn-group">
           <button type="button" class="btn">
-            Edit
-            <span class="visually-hidden">Success is the sum of small efforts - repeated day in and day out.</span>
+            Edit <span class="visually-hidden">{quote.name}</span>
           </button>
-          <button type="button" class="btn btn__danger">
-            Delete
-            <span class="visually-hidden">Success is the sum of small efforts - repeated day in and day out.</span>
-          </button>
-        </div>
-      </div>
-    </li>
-
-    <!-- quote-3 -->
-    <li class="quote">
-      <div class="stack-small">
-        <div class="c-cb">
-          <input type="checkbox" id="quote-3" />
-          <label for="quote-3" class="quote-label">
-            Strive not to be a success, but rather to be of value.
-          </label>
-        </div>
-        <div class="btn-group">
-          <button type="button" class="btn">
-            Edit
-            <span class="visually-hidden">Strive not to be a success, but rather to be of value.</span>
-          </button>
-          <button type="button" class="btn btn__danger">
-            Delete
-            <span class="visually-hidden">Strive not to be a success, but rather to be of value.</span>
+          <button type="button" class="btn btn__danger"
+            on:click={() => removeQuote(quote)}
+          >
+            Delete <span class="visually-hidden">{quote.name}</span>
           </button>
         </div>
       </div>
     </li>
+  {:else}
+    <li>Get Motivated!</li>
+  {/each}
   </ul>
 
   <hr />
 
   <!-- MoreActions -->
   <div class="btn-group">
-    <button type="button" class="btn btn__primary">Select all</button>
+    <button type="button" class="btn btn__primary">Check all</button>
     <button type="button" class="btn btn__primary">Reset all</button>
   </div>
+
 </div>
