f25afbc15a65d375c0127ed647d5790c23123a92
harryhmohney <harrison.mohney@gmail.com>
2023-09-29 11:59:44 -0700

    refactor sveltekit

--- src/components/NewQuote.svelte
+++ src/components/NewQuote.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { createEventDispatcher, onMount } from 'svelte'
   const dispatch = createEventDispatcher()
 
--- src/components/Quote.svelte
+++ src/components/Quote.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   // import { tick } from 'svelte'
   import { createEventDispatcher } from 'svelte'
   const dispatch = createEventDispatcher()
